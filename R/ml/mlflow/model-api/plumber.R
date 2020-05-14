e7be532ae6d2b69e4732bd5dbf0e75d080e4bdbe
James Blair <james.m.blair09@gmail.com>
2020-05-14 18:20:00 +0000

    ML and mlflow examples

---
+++ R/ml/mlflow/model-api/plumber.R
@@ -0,0 +1,26 @@
+library(plumber)
+library(mlflow)
+library(httr)
+
+model <- mlflow_load_model(model_uri = "models:/iris_r/2")
+
+#* @apiTitle mlflow model API
+
+#* Modify the request with already parsed data
+#* @filter add-data
+function(req, res) {
+  data <- tryCatch(jsonlite::parse_json(req$postBody, simplifyVector = TRUE),
+                   error = function(e) NULL)
+  if (is.null(data)) {
+    res$status <- 400
+    return(list(error = "No data submitted"))
+  }
+  req$data <- data
+  forward()
+}
+
+#* Return predicted values from R model
+#* @post /predict/r
+function(req, res) {
+  mlflow_predict(model, req$data)
+}
